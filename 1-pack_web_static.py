4b5ea2539984f7292694ece00f0e3aff19900597
DevDecor <fiyiziyech@gmail.com>
2023-07-10 03:05:11 +0100

    Do pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-from fabric.api import *
+"""Deployment scripts"""
+from fabric.api import local
 from datetime import datetime
-import tarfile
-
-# env.hosts = [
-#     '35.153.93.137',
-#     '54.236.33.80'
-#   # 'server2.domain.tld',
-# ]
-# # Set the username
-# env.user   = "ubuntu"
-
 
 def do_pack():
-    source = "web_static"
+    """Create a .tgz archive from the web_static folder."""
     now = datetime.now()
-    of = f"versions/web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(timestamp)
+
     local("mkdir -p versions")
-    result = local(f"tar -czvf {of} web_static")
+
+    result = local("tar -czvf {} web_static".format(archive_path))
     if result.failed:
         return None
-    return of
+
+    return archive_path
